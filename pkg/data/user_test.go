b452931faee9040c3b02d5e3be4a5afe2a19d9e4
souhub <ace0804sou@gmail.com>
2020-10-11 10:03:54 +0900

    Add the user test

--- pkg/data/user_test.go
+++ pkg/data/user_test.go
@@ -1,8 +1,28 @@
 package data
 
-TestUser(t *testing.T){
+import "testing"
 
-	t.Run("Create the user",func(t *testing.T){
+var user=User{
+	Name: "Taro",
+	UserIdStr: "taroId",
+	Email: "taro@gmail.com",
+	Password: "taroPass",
+	ImagePath: "default.png",
+}
 
-	})
+func TestCreate(t *testing.T){
+	if err:=user.Create(); err!=nil{
+		t.Fatal(err)
+	}
+
+	gotUser,err:=UserByUserIdStr(user.UserIdStr)
+	if err!=nil{
+		t.Fatal(err)
+	}
+
+	got :=gotUser.Id
+	want:=1
+	if got!=want{
+		t.Errorf("expected %d but got %d",want,got)
+	}
 }
