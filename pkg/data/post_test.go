caee2e7aabeab2bd29d780909eb9448690989f30
souhub <ace0804sou@gmail.com>
2020-10-13 07:34:30 +0900

    Add the post tests

--- pkg/data/post_test.go
+++ pkg/data/post_test.go
@@ -2,6 +2,8 @@ package data
 
 import (
 	"testing"
+
+	"github.com/google/uuid"
 )
 
 func TestPost(t *testing.T) {
@@ -29,6 +31,64 @@ func TestPost(t *testing.T) {
 		assertCorrectMessage(t, want, got)
 	})
 
+	// Update test
+	t.Run("UpdatePost", func(t *testing.T) {
+		reset(t)
+		err := post.Create()
+		if err != nil {
+			t.Fatal(err)
+		}
+		gotPost, err := PostByUuid(post.Uuid)
+		if err != nil {
+			t.Fatal(err)
+		}
+		want := map[string]string{
+			"title": "New title",
+			"body":  "New body",
+		}
+		gotPost.Title = want["title"]
+		gotPost.Body = want["body"]
+		err = gotPost.UpdatePost()
+		if err != nil {
+			t.Error(err, "- Failed to update the user.")
+		}
+		updatedUser, err := PostByUuid(post.Uuid)
+		if err != nil {
+			t.Fatal(err)
+		}
+		got := Post{
+			Title: updatedUser.Title,
+			Body:  updatedUser.Body,
+		}
+		if want["title"] != got.Title || want["body"] != got.Body {
+			t.Error(err, "- Failed to update the user.")
+		}
+	})
+
+	// UpdateThumbnail
+	t.Run("UpdateThumbnail", func(t *testing.T) {
+		reset(t)
+		err := post.Create()
+		if err != nil {
+			t.Fatal(err)
+		}
+		gotPost, err := PostByUuid(post.Uuid)
+		if err != nil {
+			t.Fatal(err)
+		}
+		want := "new-thumbnailpath"
+		gotPost.ThumbnailPath = want
+		if err := gotPost.UpdateThumbnail(); err != nil {
+			t.Error(err, "- Failed to update the thumbnail.")
+		}
+		updatedPost, err := PostByUuid(post.Uuid)
+		if err != nil {
+			t.Fatal(err)
+		}
+		got := updatedPost.ThumbnailPath
+		assertCorrectMessage(t, want, got)
+	})
+
 	// Delete test
 	t.Run("Delete", func(t *testing.T) {
 		reset(t)
@@ -44,4 +104,40 @@ func TestPost(t *testing.T) {
 			t.Error(err, "- Failed to delete the post.")
 		}
 	})
+
+	// ResetPosts test
+	t.Run("ResetPosts", func(t *testing.T) {
+		reset(t)
+		posts := []Post{
+			{
+				Uuid:          uuid.New().String(),
+				Title:         "Hello",
+				Body:          "Hello World",
+				UserId:        user.Id,
+				UserIdStr:     user.UserIdStr,
+				UserName:      user.Name,
+				ThumbnailPath: "default_thumbnail.jpg",
+				CreatedAt:     "2020-10-10",
+			},
+			{
+				Uuid:          uuid.New().String(),
+				Title:         "Good morning",
+				Body:          "Good mouning Good morning",
+				UserId:        user.Id,
+				UserIdStr:     user.UserIdStr,
+				UserName:      user.Name,
+				ThumbnailPath: "default_thumbnail.jpg",
+				CreatedAt:     "2020-10-10",
+			},
+		}
+		for _, post := range posts {
+			if err := post.Create(); err != nil {
+				t.Fatal(err)
+			}
+		}
+		if err := ResetPosts(); err != nil {
+			t.Error(err, "- Failed to delete all of the posts.")
+		}
+	})
+
 }
